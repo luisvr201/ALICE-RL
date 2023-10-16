31ebf32be4d48f21f778ab3bc1702f1ab370b032
luisvr201 <119078851+luisvr201@users.noreply.github.com>
2023-10-16 18:46:35 -0500

    Update maidmap.js

--- js/maidmap.js
+++ js/maidmap.js
@@ -532,13 +532,24 @@ async function add_node() {
     return;
   }
 
-  // Llame a la función get_children_suggestions y espere la respuesta
-    const response = await get_children_suggestions(selected_node, _jm, document.getElementById("child-node-suggestion").value);
+async function get_embedded_response(selected_node, _jm, inputValue) {
+  try {
+    const response = await get_children_suggestions(selected_node, _jm, inputValue);
 
-        if (response.innerHTML.indexOf('<embed>') === -1 || response.innerHTML.indexOf('</embed>') === -1) {
+    // Verifica si response ya contiene etiquetas <embed></embed>
+    if (response.indexOf('<embed>') === -1 || response.indexOf('</embed>') === -1) {
       // Si alguna de las etiquetas no está presente, agrégalas al principio y al final del contenido
-          response.innerHTML = '<embed>' + response.innerHTML + '</embed>';
+      return '<embed>' + response + '</embed>';
     }
+
+    return response; // Si ya contiene las etiquetas <embed>, no hagas cambios
+  } catch (error) {
+    console.error('Error en get_embedded_response:', error);
+    return ''; // Manejar errores según tus necesidades
+  }
+}
+  // Llame a la función get_children_suggestions y espere la respuesta
+    // const response = await get_children_suggestions(selected_node, _jm, document.getElementById("child-node-suggestion").value);
     
   console.log(response);
     // Llame a la función ProcessGptResponse para procesar la respuesta y agregar nodos
